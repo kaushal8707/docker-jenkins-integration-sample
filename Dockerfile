5300ce1cafc4532cd9cdbbaaa3d8c268d63071d0
Kaushal Kumar <kaukumar3@publicisgroupe.net>
2023-06-13 18:04:06 +0530

    added docker file

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:8
+EXPOSE 8080
+ADD target/docker-jenkins-integration-sample.jar docker-jenkins-integration-sample
+ENTRYPOINT ["java","-jar","/docker-jenkins-integration-sample.jar"]
+
+
+
+
--- src/main/java/com/java/selfdeveloped/cloud/jenkins/api/DockerJenkinsIntegrationSampleApplication.java
+++ src/main/java/com/java/selfdeveloped/cloud/jenkins/api/DockerJenkinsIntegrationSampleApplication.java
@@ -1,13 +1,16 @@
 package com.java.selfdeveloped.cloud.jenkins.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinsIntegrationSampleApplication {
-
+	static Logger log=LoggerFactory.getLogger(DockerJenkinsIntegrationSampleApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+		log.info("My app is starting....."); 
 	}
 
 }
