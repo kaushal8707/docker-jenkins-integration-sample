58b12edbac8201161a03803743da392d5dfc1a0b
Kaushal Kumar <kaukumar3@publicisgroupe.net>
2023-06-13 16:56:28 +0530

    first commit

---
+++ src/main/java/com/java/selfdeveloped/cloud/jenkins/api/DockerJenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.java.selfdeveloped.cloud.jenkins.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+	}
+
+}
